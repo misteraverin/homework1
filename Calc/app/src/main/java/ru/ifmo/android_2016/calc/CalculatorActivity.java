89013df07d28190565a5ad2a6fa230db84ddb135
Averin Maxim <amax0703@gmail.com>
2016-09-28 22:30:39 +0300

    Small bug fixes
    1. Fix problem with point
    2. Replace StringBuilder on String
    3. Fix long if-statement

--- Calc/app/src/main/java/ru/ifmo/android_2016/calc/CalculatorActivity.java
+++ Calc/app/src/main/java/ru/ifmo/android_2016/calc/CalculatorActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.widget.Button;
 import android.view.View;
 import android.widget.TextView;
+import android.text.TextUtils;
 
 
 public class CalculatorActivity extends AppCompatActivity {
@@ -21,9 +22,10 @@ public class CalculatorActivity extends AppCompatActivity {
     private final int DIV = 4;
 
 
-    private StringBuilder currentText = new StringBuilder("0");
+    private String currentText = "0";
     int currentState = EMPTY;
     int currentOperation = DIGIT;
+    private boolean hasPoint = false;
     TextView expression;
     double firstNumber = 0, secondNumber = 0;
 
@@ -32,6 +34,7 @@ public class CalculatorActivity extends AppCompatActivity {
     static final String SECOND_NUMBER = "secondNumber";
     static final String STATE = "currentState";
     static final String CURRENT_OPERATION = "currentOperation";
+    static final String HAS_POINT = "hasNumberPoint";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,49 +44,47 @@ public class CalculatorActivity extends AppCompatActivity {
         if (savedInstanceState != null) {
             firstNumber = savedInstanceState.getDouble(FIRST_NUMBER);
             secondNumber = savedInstanceState.getDouble(SECOND_NUMBER);
-            currentText = new StringBuilder(savedInstanceState.getString(CURRENT_STRING));
+            currentText = new String(savedInstanceState.getString(CURRENT_STRING));
             currentOperation = savedInstanceState.getInt(CURRENT_OPERATION);
             currentState = savedInstanceState.getInt(STATE);
-            expression.setText(currentText.toString());
+            hasPoint = savedInstanceState.getBoolean(HAS_POINT);
+            expression.setText(currentText);
         }
-        expression.setText(currentText.toString());
+        expression.setText(currentText);
     }
 
     public void onClickDigit(View view) {
         Button button = (Button) view;
         currentState = DIGIT;
-        if (currentText.length() == 1 && button.getText().toString().charAt(0) == '0' && currentText.charAt(0) == '0') {
-            // I don't know how to write effective if-statement, sorry ;)
-        } else if (currentText.length() == 1 && button.getText().toString().charAt(0) != '0' && currentText.charAt(0) == '0') {
-            currentText.setLength(0);
-            currentText.append((button.getText().toString()));
-            expression.setText(currentText.toString());
-        } else {
-            currentText.append(button.getText().toString());
-            expression.setText(currentText.toString());
+        if (TextUtils.equals("0", currentText) && button.getId() != R.id.d0) {
+            currentText = button.getText().toString();
+        } else if (!TextUtils.equals("0", currentText)) {
+            currentText = currentText + button.getText().toString();
         }
+        expression.setText(currentText);
         if (currentOperation != EMPTY) {
-            secondNumber = Double.parseDouble(currentText.toString());
+            secondNumber = Double.parseDouble(currentText);
         } else {
-            firstNumber = Double.parseDouble(currentText.toString());
+            firstNumber = Double.parseDouble(currentText);
         }
     }
 
     public void onClickClear(View view) {
-        currentText.setLength(0);
-        currentText.append('0');
-        expression.setText(currentText.toString());
+        currentText = "0";
+        hasPoint = false;
+        expression.setText(currentText);
         resetState();
     }
 
     public void OnClickPoint(View view) {
-        if (currentState != OPERATION) {
-            currentText.append('.');
-            expression.setText(currentText.toString());
+        if (currentState != OPERATION && hasPoint == false) {
+            currentText = currentText + ".";
+            hasPoint = true;
+            expression.setText(currentText);
             if (currentOperation != EMPTY) {
-                secondNumber = Double.parseDouble(currentText.toString());
+                secondNumber = Double.parseDouble(currentText);
             } else {
-                firstNumber = Double.parseDouble(currentText.toString());
+                firstNumber = Double.parseDouble(currentText);
             }
         }
     }
@@ -91,27 +92,30 @@ public class CalculatorActivity extends AppCompatActivity {
     public void OnClickDelete(View view) {
         if (currentState == OPERATION) {
             expression.setText("0");
-            currentText.setLength(0);
+            currentText = "";
+            hasPoint = false;
             resetState();
             return;
         }
 
         if (currentText.length() > 0) {
-            currentText.deleteCharAt(currentText.length() - 1);
+            if (currentText.charAt(currentText.length() - 1) == '.')
+                hasPoint = false;
+            currentText = currentText.substring(0, currentText.length() - 1);
         }
-        expression.setText(currentText.toString());
+        expression.setText(currentText);
 
 
         if (currentOperation != EMPTY) {
             if (currentText.length() == 0) {
                 secondNumber = 0;
             } else
-                secondNumber = Double.parseDouble(currentText.toString());
+                secondNumber = Double.parseDouble(currentText);
         } else {
             if (currentText.length() == 0) {
                 firstNumber = 0;
             } else
-                firstNumber = Double.parseDouble(currentText.toString());
+                firstNumber = Double.parseDouble(currentText);
         }
 
 
@@ -119,16 +123,16 @@ public class CalculatorActivity extends AppCompatActivity {
 
     public void OnClickPlusMinus(View view) {
         if (currentText.charAt(0) == '-')
-            currentText.deleteCharAt(0);
+            currentText = currentText.substring(1);
         else if (currentText.length() > 0 && currentText.charAt(0) != '0')
-            currentText.insert(0, '-');
+            currentText = "-" + currentText;
         else if (currentText.length() > 1 && currentText.charAt(0) == '0')
-            currentText.insert(0, '-');
-        expression.setText(currentText.toString());
+            currentText = "-" + currentText;
+        expression.setText(currentText);
         if (currentOperation != EMPTY) {
-            secondNumber = Double.parseDouble(currentText.toString());
+            secondNumber = Double.parseDouble(currentText);
         } else {
-            firstNumber = Double.parseDouble(currentText.toString());
+            firstNumber = Double.parseDouble(currentText);
         }
     }
 
@@ -150,8 +154,9 @@ public class CalculatorActivity extends AppCompatActivity {
         Button button = (Button) view;
         currentOperation = chooseOperation(button.getText().toString());
         currentState = OPERATION;
-        currentText.setLength(0);
-        expression.setText(currentText.toString());
+        currentText = "";
+        hasPoint = false;
+        expression.setText(currentText);
     }
 
     public void OnClickResult(View view) {
@@ -169,7 +174,7 @@ public class CalculatorActivity extends AppCompatActivity {
             case DIV:
                 if (Math.abs(secondNumber) < 1e-9) {
                     expression.setText("ERROR");
-                    currentText.setLength(0);
+                    currentText = "";
                     resetState();
                     firstNumber = result;
                 } else {
@@ -178,7 +183,7 @@ public class CalculatorActivity extends AppCompatActivity {
                 break;
         }
         if (!expression.getText().equals("ERROR")) {
-            currentText = new StringBuilder(String.valueOf(result));
+            currentText = String.valueOf(result);
             expression.setText(currentText);
             resetState();
             firstNumber = result;
@@ -189,14 +194,16 @@ public class CalculatorActivity extends AppCompatActivity {
         currentOperation = EMPTY;
         currentState = OPERATION;
         firstNumber = 0;
+        hasPoint = false;
         secondNumber = 0;
     }
 
     @Override
     public void onSaveInstanceState(Bundle saveInstanceState) {
         super.onSaveInstanceState(saveInstanceState);
-        saveInstanceState.putString(CURRENT_STRING, currentText.toString());
+        saveInstanceState.putString(CURRENT_STRING, currentText);
         saveInstanceState.putDouble(FIRST_NUMBER, firstNumber);
+        saveInstanceState.putBoolean(HAS_POINT, hasPoint);
         saveInstanceState.putDouble(SECOND_NUMBER, secondNumber);
         saveInstanceState.putInt(STATE, currentState);
         saveInstanceState.putInt(CURRENT_OPERATION, currentOperation);
